9672743e5a0c16b671272692662dcbaff528694c
SincereSnail <127372470+SincereSnail@users.noreply.github.com>
2024-08-23 15:45:09 +0900

    Update st_utils.py

--- utils/st_utils.py
+++ utils/st_utils.py
@@ -3,6 +3,7 @@ from utils.rag_utils import *
 import time
 
 def print_msg(prompt, retriever, rag_prompt_custom, llm):
+    print(st.session_state.chat_history)
 # 사용자 메시지 추가
     for chat in st.session_state.chat_history:
         with st.chat_message("user" if chat["role"] == "user" else "ai"):
@@ -21,10 +22,16 @@ def print_msg(prompt, retriever, rag_prompt_custom, llm):
 
 # AI 응답을 스트리밍 방식으로 표시
 def streaming_md():
+    c = 0
     for chat in st.session_state.chat_history:
-        with st.chat_message("user" if chat["role"] == "user" else "ai"):
-            stream_text(chat["content"])
-            
+        if c<=len(st.session_state.chat_history)-2 :
+            with st.chat_message("user" if chat["role"] == "user" else "ai"):
+                st.markdown(chat["content"])
+        else  :
+            with st.chat_message("ai"):
+                stream_text(chat["content"])
+        c += 1
+
 def stream_text(text):    
     response_placeholder = st.empty()
     response_text = ""
